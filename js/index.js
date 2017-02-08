f0dd4062e03e95c2e2ee3b1ac48fdd9f99db701e
Jonah Chazan <person.1123.loztp@gmail.com>
2017-02-07 20:16:39 -0500

    bug fixes

--- js/index.js
+++ js/index.js
@@ -32,8 +32,18 @@ $(document).ready(function(){
 
 var backgroundArray = ["#1","#2","#3","#4","#5","#6"];
 
+$(document).ready(function() {
+  var section = ($(document).height() - window.screen.height)/5;
+  var index = Math.floor(document.body.scrollTop/section);
+  for (var i = 0; i < index; i++) {
+	$(backgroundArray[i]).css({'opacity': 0});
+  }
+  var opacity = Math.round((1-((document.body.scrollTop/section) - index))*100)/100;
+  $(backgroundArray[index]).css({'opacity': opacity});
+});
+
 window.onscroll = function(){
-  var section = $(document).height()/5;
+  var section = ($(document).height() - window.screen.height)/5
   var index = Math.floor(document.body.scrollTop/section);
   var opacity = Math.round((1-((document.body.scrollTop/section) - index))*100)/100;
   $(backgroundArray[index]).css({'opacity': opacity});
